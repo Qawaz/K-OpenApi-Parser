9d5f20fe740bcaad693955533c7bbe9b2b0624f2
Waqas Tahir <wakaztahir@gmail.com>
2023-04-02 17:38:18 +0500

    updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,8 @@ dependencies {
     implementation("javax.mail:javax.mail-api:1.6.1")
     implementation("com.sun.mail:javax.mail:1.6.1")
     implementation("javax.annotation:javax.annotation-api:1.3.2")
-    testImplementation("junit:junit:4.12")
-    testImplementation("com.google.guava:guava:19.0")
+    testImplementation("junit:junit:4.13.1")
+    testImplementation("com.google.guava:guava:30.0-jre")
     testImplementation("org.skyscreamer:jsonassert:1.5.0")
     testImplementation("org.apache.commons:commons-lang3:3.7")
 }
--- json-overlay/build.gradle.kts
+++ json-overlay/build.gradle.kts
@@ -1,6 +1,3 @@
-import java.util.Properties;
-import java.io.FileInputStream;
-
 plugins {
     java
     kotlin("jvm")
@@ -17,9 +14,7 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
     implementation("commons-cli:commons-cli:1.4")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:$jacksonVersion")
-    implementation("com.github.javaparser:javaparser-core:3.5.7")
     implementation("javax.annotation:javax.annotation-api:1.3.2")
-    implementation("org.eclipse.xtend:org.eclipse.xtend.lib:2.17.1")
-    testImplementation("junit:junit:4.12")
+    testImplementation("junit:junit:4.13.1")
 
 }
