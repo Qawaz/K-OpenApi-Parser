f25458e974ea5466913e11cab72ff7fd7779ced7
Waqas Tahir <wakaztahir@gmail.com>
2023-04-05 17:49:20 +0500

    remove extra dependency

--- json-overlay/build.gradle.kts
+++ json-overlay/build.gradle.kts
@@ -13,7 +13,6 @@ dependencies {
 
     implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:$jacksonVersion")
-    implementation("javax.annotation:javax.annotation-api:1.3.2")
     implementation("com.wakaztahir:kate:1.0.1")
     testImplementation("junit:junit:4.13.1")
 
