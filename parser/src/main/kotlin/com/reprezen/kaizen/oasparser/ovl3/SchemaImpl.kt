bd8d1e1292847691c6e6d5c4f0da7a954ac84d8f
Waqas Tahir <wakaztahir@gmail.com>
2023-04-15 16:39:52 +0500

    update

--- parser/src/main/kotlin/com/reprezen/kaizen/oasparser/ovl3/SchemaImpl.kt
+++ parser/src/main/kotlin/com/reprezen/kaizen/oasparser/ovl3/SchemaImpl.kt
@@ -3,20 +3,29 @@ package com.reprezen.kaizen.oasparser.ovl3
 import com.reprezen.kaizen.oasparser.model3.*
 import com.reprezen.jsonoverlay.MapOverlay
 import com.reprezen.jsonoverlay.StringOverlay
+import com.reprezen.jsonoverlay.ListOverlay
 import com.reprezen.jsonoverlay.OverlayFactory
 import com.reprezen.jsonoverlay.Builder
 import com.reprezen.jsonoverlay.JsonPointer
+import com.reprezen.kaizen.oasparser.ovl3.XmlImpl
 import com.reprezen.jsonoverlay.ReferenceManager
 import com.reprezen.jsonoverlay.IntegerOverlay
+import com.reprezen.kaizen.oasparser.ovl3.SchemaImpl
 import kotlin.collections.List
 import com.reprezen.jsonoverlay.JsonOverlay
 import com.reprezen.jsonoverlay.Overlay
+import java.util.stream.Collectors
+import com.reprezen.kaizen.oasparser.ovl3.DiscriminatorImpl
 import com.reprezen.jsonoverlay.parser.Generated
+import java.util.Optional
+import com.reprezen.jsonoverlay.IJsonOverlay
 import com.reprezen.jsonoverlay.PropertiesOverlay
 import com.reprezen.jsonoverlay.NumberOverlay
 import kotlinx.serialization.json.JsonElement
 import com.reprezen.jsonoverlay.ObjectOverlay
+import kotlin.collections.Map
 import com.reprezen.jsonoverlay.BooleanOverlay
+import com.reprezen.kaizen.oasparser.ovl3.ExternalDocsImpl
 
 class SchemaImpl : PropertiesOverlay<Schema> ,Schema {
 
